632ed638df8cc13f4edce69b50c7b2eab3ff768b
Vaibhavi Singh <vaibhavisingh132006@gmail.com>
2026-06-20 23:56:57 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,13 +34,17 @@ public class Main {
             } else if (command.equals("cd")) {
                 if (parts.length > 1) {
                     String targetPath = parts[1];
-                    Path basePat = Paths.get(currentDir);
-                    Path resolvedPath = basePat.resolve(targetPath).normalize();
-                    File targetDir = resolvedPath.toFile();
-                    
-                    if (targetDir.exists() && targetDir.isDirectory()) {
-                        currentDir = resolvedPath.toString();
-                    } else {
+                    try {
+                        Path basePat = Paths.get(currentDir);
+                        Path resolvedPath = basePat.resolve(targetPath).normalize();
+                        File targetDir = resolvedPath.toFile();
+                        
+                        if (targetDir.exists() && targetDir.isDirectory()) {
+                            currentDir = resolvedPath.toString();
+                        } else {
+                            System.out.println("cd: " + targetPath + ": No such file or directory");
+                        }
+                    } catch (Exception e) {
                         System.out.println("cd: " + targetPath + ": No such file or directory");
                     }
                 }
