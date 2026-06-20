da792d4415601dc06791a345319cfa5b91c971ae
Vaibhavi Singh <vaibhavisingh132006@gmail.com>
2026-06-20 23:43:25 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
@@ -23,7 +27,24 @@ public class Main {
                 if (arg.equals("echo") || arg.equals("exit") || arg.equals("type")) {
                     System.out.println(arg + " is a shell builtin");
                 } else {
-                    System.out.println(arg + ": not found");
+                    String pathEnv = System.getenv("PATH");
+                    boolean found = false;
+                    
+                    if (pathEnv != null) {
+                        String[] directories = pathEnv.split(File.pathSeparator);
+                        for (String dir : directories) {
+                            Path fullPath = Paths.get(dir, arg);
+                            if (Files.isExecutable(fullPath)) {
+                                System.out.println(arg + " is " + fullPath.toString());
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if (!found) {
+                        System.out.println(arg + ": not found");
+                    }
                 }
             } else {
                 System.out.println(input + ": command not found");
